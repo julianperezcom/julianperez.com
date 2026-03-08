c546aeff5769ecb3ed4fdf68e45c19a257975e97
MB7 <mb7@MB7.local>
2026-03-08 15:49:10 +0100

    chore: add root sanity.config.ts and studio script

---
+++ sanity.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'sanity'
+import { structureTool } from 'sanity/structure'
+import { visionTool } from '@sanity/vision'
+import { schemas } from './sanity/schemas'
+
+export default defineConfig({
+  name: 'julianperez',
+  title: 'Julian Perez CMS',
+  projectId: 'esbhlkf9',
+  dataset: 'production',
+  plugins: [structureTool(), visionTool()],
+  schema: { types: schemas },
+})
